66c24e8bb0c47ecaf4d4f09d2a5b7b3540c474e9
Mathieu Dumoulin <mathieu.dumoulin@outlook.com>
2018-05-18 14:35:50 -0400

    Setting up the repo for a new deployment

--- tracking-server/dist/src/app/models/user-profile.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var UserProfile = (function () {
-    function UserProfile() {
-    }
-    return UserProfile;
-}());
-exports.UserProfile = UserProfile;
-//# sourceMappingURL=user-profile.model.js.map
